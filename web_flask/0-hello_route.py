f152639ad22c4a9e5e9e216fe3f0e4ff070d8827
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-07-24 04:41:14 +0300

    Create 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Flask Web Application
+
+This script starts a Flask web application that listens on 0.0.0.0, port 5000.
+It defines a single route that displays "Hello HBNB!" when accessed.
+
+Routes:
+    /: Display "Hello HBNB!"
+
+Usage:
+    Run this script and visit http://0.0.0.0:5000/ in your web browser.
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!' on the webpage."""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
